93bfe04d7b66b64659915e6c26c0008f87f446d4
jleroux751 <jasmin.leroux@hotmail.com>
2026-06-07 08:57:02 -0400

    correct main

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_string" "myrandom" {
   length = 6
   upper = false 
   special = false
-  number = false   
+  numeric = false   
 }
 
 # Create Resource Group
@@ -21,15 +21,17 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_storage_account" "storage_account" {
   name                = "${var.storage_account_name}${random_string.myrandom.id}"
   resource_group_name = azurerm_resource_group.resource_group.name
- 
+
   location                 = var.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
   account_kind             = var.storage_account_kind
- 
-  static_website {
-    index_document = var.static_website_index_document
-    error_404_document = var.static_website_error_404_document  
-  }
+
+ }
+
+resource "azurerm_storage_account_static_website" "example" {
+  storage_account_id = azurerm_storage_account.storage_account.id
+  error_404_document = "error .html"
+  index_document     = "index.html"
 }
 
